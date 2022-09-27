9da598871cd12359d671c0699548fd9c1d50bcf1
Tomat05 <wetherill.t@outlook.com>
2022-09-27 21:43:45 +0100

    Ball does indeed bounce

--- game.cpp
+++ game.cpp
@@ -1,45 +1,111 @@
 #define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
 
-// Screen dimensions
-int WIDTH = 1280;
-int HEIGHT = 720;
+const int WINDOW_WIDTH = 1280;
+const int WINDOW_HEIGHT = 720;
 
-// Override base class with your custom functionality
-class Game : public olc::PixelGameEngine
-{
-public:
-	Game()
-	{
-		// Name your application
-		sAppName = "Name Me";
-	}
+class Game : public olc::PixelGameEngine {
+ public:
+  Game() { sAppName = "Ball lol"; }
 
-public:
-    // Called once at the start
-	bool OnUserCreate() override
-	{
+ private:
+  float timer = 0;
+  int frames = 0;
+  int fps;
 
-		return true;
-	}
+  float ballY = 50;
+  float ballX = 50;
+  float ballSpeedY = 300;
+  float ballSpeedX = 300;
 
-    // Called once per frame
-	bool OnUserUpdate(float fElapsedTime) override
-	{
+ public:
+  bool OnUserCreate() override {
+    /*
+      Load resources here
+    */
+    return true;
+  }
 
-		for (int x = 0; x < ScreenWidth(); x++)
-			for (int y = 0; y < ScreenHeight(); y++)
-				Draw(x, y, olc::Pixel(x % 256, y * 255, 0));
-		return true;
-	}
+  bool OnUserUpdate(float fElapsedTime) override {
+
+    ballY += ballSpeedY * GetElapsedTime();
+    ballX += ballSpeedX * GetElapsedTime(); 
+
+    if (ballY >= WINDOW_HEIGHT - 35 || ballY <= 35)
+    {
+        ballSpeedY = -ballSpeedY;
+    }
+
+    if (ballX >= WINDOW_WIDTH - 35 || ballX <= 35)
+    {
+        ballSpeedX = -ballSpeedX;
+    }
+    
+
+    timer += fElapsedTime;
+    frames++;
+    if (timer > 1.0) {
+      fps = frames;
+      frames = 0;
+      timer -= 1;
+    }
+
+    inputs();
+    processes();
+    outputs();
+
+    if (GetKey(olc::Key::ESCAPE).bPressed) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  void inputs() {
+    /*
+      Game controls goes here
+    */
+
+    if (GetKey(olc::Key::SPACE).bHeld) {
+      std::cout << "Space!" << std::endl;
+    }
+    if (GetMouse(0).bHeld) {
+      std::cout << "Click!" << std::endl;
+    }
+  }
+
+  void processes() {
+    /*
+      Game logic goes here
+    */
+  }
+
+  void outputs() {
+    SetPixelMode(olc::Pixel::NORMAL);
+
+    Clear(olc::DARK_BLUE);
+
+    FillCircle(ballX, ballY, 20, olc::RED);
+
+    DrawLine(15, 15, WINDOW_WIDTH - 15, 15, olc::WHITE);
+	DrawLine(WINDOW_WIDTH - 15, 15, WINDOW_WIDTH - 15, WINDOW_HEIGHT - 15, olc::WHITE);
+	DrawLine(WINDOW_WIDTH - 15, WINDOW_HEIGHT - 15, 15, WINDOW_HEIGHT - 15, olc::WHITE);
+    DrawLine(15, WINDOW_HEIGHT - 15, 15, 15, olc::WHITE);
+
+    if (fps > 0) {
+      auto fpsPosition = olc::vi2d(10, 10);
+      DrawStringDecal(fpsPosition, "FPS " + std::to_string(fps));
+    }
+  }
+
+  bool OnUserDestroy() override {
+    std::cout << "Closing game" << std::endl;
+    return true;
+  }
 };
 
-
-
-int main()
-{
-	Game game;
-	if (game.Construct(WIDTH, HEIGHT, 1, 1))
-		game.Start();
-	return 0;
+int main() {
+  Game game;
+  if (game.Construct(WINDOW_WIDTH, WINDOW_HEIGHT, 1, 1)) game.Start();
+  return 0;
 }
