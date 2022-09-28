5b6b0f5bc02ed09dd6237648b0e334f46ea8f730
Tomat05 <wetherill.t@outlook.com>
2022-09-28 09:20:22 +0100

    Added TODO

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Bouncing Ball
+
+## TODO
+- More balls
+- Partition space by balls to form bounding boxes
+- Collisions based on bounding box interception
+- Collisions based on physics (conservation of momentum, etc.)
--- game.cpp
+++ game.cpp
@@ -1,111 +1,96 @@
 #define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
 
-const int WINDOW_WIDTH = 1280;
-const int WINDOW_HEIGHT = 720;
+const int WIDTH = 1280;
+const int HEIGHT = 720;
+
+// class Ball {
+//     float ballY = 50;
+//     float ballX = 50;
+//     float ballSpeedY = 300;
+//     float ballSpeedX = 300;
+// };
 
 class Game : public olc::PixelGameEngine {
  public:
-  Game() { sAppName = "Ball lol"; }
+	Game() { sAppName = "Ball lol"; }
 
  private:
-  float timer = 0;
-  int frames = 0;
-  int fps;
+	float timer = 0;
+	int frames = 0;
+	int fps;
 
-  float ballY = 50;
-  float ballX = 50;
-  float ballSpeedY = 300;
-  float ballSpeedX = 300;
+	float ballY = 50;
+    float ballX = 50;
+    float ballSpeedY = 300;
+    float ballSpeedX = 300;
 
  public:
-  bool OnUserCreate() override {
-    /*
-      Load resources here
-    */
-    return true;
-  }
+	bool OnUserCreate() override {
+		/*
+    	Load resources here
+    	*/
+    	return true;
+	}
 
-  bool OnUserUpdate(float fElapsedTime) override {
+	bool OnUserUpdate(float fElapsedTime) override {
+    	timer += fElapsedTime;
+    	frames++;
+    	if (timer > 1.0) {
+      		fps = frames;
+      		frames = 0;
+      		timer -= 1;
+    	}
 
-    ballY += ballSpeedY * GetElapsedTime();
-    ballX += ballSpeedX * GetElapsedTime(); 
+    	processes();
+    	outputs();
 
-    if (ballY >= WINDOW_HEIGHT - 35 || ballY <= 35)
-    {
-        ballSpeedY = -ballSpeedY;
-    }
+    	if (GetKey(olc::Key::ESCAPE).bPressed) {
+    		return false;
+    	} else {
+      		return true;
+    	}
+	}
 
-    if (ballX >= WINDOW_WIDTH - 35 || ballX <= 35)
-    {
-        ballSpeedX = -ballSpeedX;
-    }
-    
+	void processes() {
+    	ballY += ballSpeedY * GetElapsedTime();
+		ballX += ballSpeedX * GetElapsedTime(); 
 
-    timer += fElapsedTime;
-    frames++;
-    if (timer > 1.0) {
-      fps = frames;
-      frames = 0;
-      timer -= 1;
-    }
+		if (ballY >= HEIGHT - 35 || ballY <= 35) {
+			ballSpeedY = -ballSpeedY;
+		}
 
-    inputs();
-    processes();
-    outputs();
+    	if (ballX >= WIDTH - 35 || ballX <= 35) {
+        	ballSpeedX = -ballSpeedX;
+    	}
+  	}
 
-    if (GetKey(olc::Key::ESCAPE).bPressed) {
-      return false;
-    } else {
-      return true;
-    }
-  }
+  	void outputs() {
+    	SetPixelMode(olc::Pixel::NORMAL);
 
-  void inputs() {
-    /*
-      Game controls goes here
-    */
+    	Clear(olc::DARK_BLUE);
 
-    if (GetKey(olc::Key::SPACE).bHeld) {
-      std::cout << "Space!" << std::endl;
-    }
-    if (GetMouse(0).bHeld) {
-      std::cout << "Click!" << std::endl;
-    }
-  }
+    	FillCircle(ballX, ballY, 20, olc::RED);
 
-  void processes() {
-    /*
-      Game logic goes here
-    */
-  }
+    	DrawLine(15, 15, WIDTH - 15, 15, olc::WHITE);
+    	DrawLine(WIDTH - 15, 15, WIDTH - 15, HEIGHT - 15, olc::WHITE);
+    	DrawLine(WIDTH - 15, HEIGHT - 15, 15, HEIGHT - 15, olc::WHITE);
+    	DrawLine(15, HEIGHT - 15, 15, 15, olc::WHITE);
 
-  void outputs() {
-    SetPixelMode(olc::Pixel::NORMAL);
+    	if (fps > 0) {
+      		auto fpsPosition = olc::vi2d(10, 10);
+      		DrawStringDecal(fpsPosition, "FPS " + std::to_string(fps));
+    	}
+  	}
 
-    Clear(olc::DARK_BLUE);
-
-    FillCircle(ballX, ballY, 20, olc::RED);
-
-    DrawLine(15, 15, WINDOW_WIDTH - 15, 15, olc::WHITE);
-	DrawLine(WINDOW_WIDTH - 15, 15, WINDOW_WIDTH - 15, WINDOW_HEIGHT - 15, olc::WHITE);
-	DrawLine(WINDOW_WIDTH - 15, WINDOW_HEIGHT - 15, 15, WINDOW_HEIGHT - 15, olc::WHITE);
-    DrawLine(15, WINDOW_HEIGHT - 15, 15, 15, olc::WHITE);
-
-    if (fps > 0) {
-      auto fpsPosition = olc::vi2d(10, 10);
-      DrawStringDecal(fpsPosition, "FPS " + std::to_string(fps));
-    }
-  }
-
-  bool OnUserDestroy() override {
-    std::cout << "Closing game" << std::endl;
-    return true;
-  }
+  	bool OnUserDestroy() override {
+    	std::cout << "Closing game" << std::endl;
+    	return true;
+  	}
 };
 
 int main() {
-  Game game;
-  if (game.Construct(WINDOW_WIDTH, WINDOW_HEIGHT, 1, 1)) game.Start();
-  return 0;
+  	Game game;
+  	if (game.Construct(WIDTH, HEIGHT, 1, 1)) game.Start();
+  	return 0;
 }
